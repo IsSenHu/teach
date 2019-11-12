f8c2ea41c7dbaad52b0a6610dcfe3e4080a0b560
unknown <1178515826@qq.com>
2019-11-12 14:32:39 +0800

    ok

---
+++ src/test/java/com/cdsen/teach/TeachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdsen.teach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
